dc480cbacee67cf3a1defc03cb3afd7a3b62ceb9
WestSopho <isaiasvelascoricardo@gmail.com>
2024-01-28 12:54:01 -0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    colors:{
+      "gradient-1-a": "#7755FF",
+      "gradient-1-b": "#2F2CE9",
+      "gradient-2-a": "#4D21C9", 
+      "gradient-2-b": "#2521C9", 
+      "yellow": "#FFB21E", 
+      "blue": "#1125D6", 
+      "green": "#00BB8F", 
+      "red": "#FF5555", 
+      "dark-navy": "#303B59", 
+      "light-blue": "#CAC9FF", 
+      "very-light-blue": "#ECF2FF",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
